a69aa02bdbd321594da770b0f516044d83a9edb1
Bisrat Tamire <biarattamire288@gmail.com>
2025-09-02 22:16:27 +0300

    eslint any error fix1

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextRequest, NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 import jwt from "jsonwebtoken";
